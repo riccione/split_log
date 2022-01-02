7ee1a2001f12790e4a895ac8b7b261105eb753db
Akio Ikeda <principewar@live.com>
2022-01-02 00:05:27 -0500

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module split_log
+
+go 1.17
---
+++ split_log.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	args := os.Args
+	filePath := args[1]
+	start := args[2]
+	end := args[3]
+	fmt.Println(filePath)
+	fmt.Println(start)
+	fmt.Println(end)
+
+	f, err := os.Open(filePath)
+	check(err)
+
+	fs := bufio.NewScanner(f)
+
+	fs.Split(bufio.ScanLines)
+	isExist := false
+
+	for fs.Scan() {
+		l := fs.Text()
+		if strings.HasPrefix(l, start) {
+			isExist = true
+			if strings.HasPrefix(l, end) {
+				break
+			}
+		}
+		if isExist {
+			fmt.Println(l)
+		}
+	}
+
+	f.Close()
+}
